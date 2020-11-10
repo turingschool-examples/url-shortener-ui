019d43314c890ba9bc55e16b3bc733aa0a21970f
Naomi Ware <nware1066@yahoo.com>
2020-11-10 11:17:03 -0700

    add test for UrlContainer headings and tags

--- src/components/UrlContainer/UrlContainter.test.js
+++ src/components/UrlContainer/UrlContainter.test.js
@@ -4,8 +4,8 @@ import { screen, render, fireEvent } from '@testing-library/react'
 import UrlContainer from './UrlContainer'
 
   describe('UrlContainer', () => {
-    it('should rendeer headings and anchor tags appropriately', () => {
-      let urls = [
+    it('should render headings and anchor tags appropriately', () => {
+      let mockUrls = [
         {
           id: 1,
           long_url: "https://images.unsplash.com/photo-1470114716159-e389f8712fda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
@@ -15,13 +15,21 @@ import UrlContainer from './UrlContainer'
         {  id: 1,
           long_url: "https://www.google.com/search?q=steampunk+images&rlz=1C5CHFA_enUS886US886&oq=&aqs=chrome.0.69i59i450l8.397114599j0j15&sourceid=chrome&ie=UTF-8",
           short_url: "http://localhost:3001/useshorturl/2",
-          title: "Random steampunk photo"}
+          title: "Random steampunk image"}
       ]
-    })
       render(
-        <UrlContainer urls={this.state.urls}/>
+        <UrlContainer urls={mockUrls}/>
       )
 
-      let heading1 = 
+      let heading1 = screen.getByRole('heading', { name: /awesome photo/i })
+      let heading2 = screen.getByRole('heading', { name: /random steampunk image/i })
+      let tag1 = screen.getByRole('link', { name: /http:\/\/localhost:3001\/useshorturl\/1/i })
+      let tag2 = screen.getByRole('link', { name: /http:\/\/localhost:3001\/useshorturl\/2/i })
+
+      expect(heading1).toBeInTheDocument()
+      expect(heading2).toBeInTheDocument()
+      expect(tag1).toBeInTheDocument()
+      expect(tag2).toBeInTheDocument()
+    })
   })
 //
