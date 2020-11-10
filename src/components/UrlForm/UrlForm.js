09077706fda1f5d36d9c9776562faec98ee82e20
Naomi Ware <nware1066@yahoo.com>
2020-11-10 10:28:36 -0700

    Fix typo in form so that url and title do not duplicate, send submitPost to form as prop

--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -15,8 +15,8 @@ class UrlForm extends Component {
   }
 
   handleSubmit = e => {
-      
-    e.preventDefault();
+
+    this.props.submitUrl(this.state.urlToShorten, this.state.title)
     this.clearInputs();
   }
 
@@ -38,8 +38,8 @@ class UrlForm extends Component {
         <input
           type='text'
           placeholder='URL to Shorten...'
-          name='title'
-          value={this.state.title}
+          name='urlToShorten'
+          value={this.state.urlToShorten}
           onChange={e => this.handleNameChange(e)}
         />
 
