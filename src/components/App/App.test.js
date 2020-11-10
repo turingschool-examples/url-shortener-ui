394736c6a26a3200f171ddf7e56c5a5916b2afc9
Hanna Kim <hannakim91@gmail.com>
2020-11-10 11:33:56 -0700

    Async render for App test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,25 +1,61 @@
-import React from 'react'
-import {render, screen} from "@testing-library/react";
+import React from 'react';
+import {render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
-import App from './App'
+import App from './App';
+import { getUrls } from '../../apiCalls';
+jest.mock('../../apiCalls.js')
 
 describe('App', () => {
-  beforeEach(() => {
+
+  describe('App initial render', () => {
+
+    it('Should render initial view of App', () => {
+      render(
+        <App />
+      )
+  
+      expect(screen.getByText('URL Shortener')).toBeInTheDocument()
+  
+      expect(screen.getByPlaceholderText('Title...')).toBeInTheDocument()
+      expect(screen.getByPlaceholderText('URL to Shorten...')).toBeInTheDocument()
+      expect(screen.getByText('Shorten Please!')).toBeInTheDocument()
+      
+      expect(screen.getByText('No urls yet! Find some to shorten!')).toBeInTheDocument()
+    })
   })
 
-  it('Should render App render', () => {
-    render(
-      <App 
-      />
-    )
-screen.debug()
-    expect(screen.getByText('URL Shortener')).toBeInTheDocument()
+  describe('Async render', () => {
+    beforeEach(() => {
+      getUrls.mockResolvedValue({
+        urls: [
+            {
+              id: 1,
+              long_url: "iamaveryloooongurl.com",
+              short_url: "short.co",
+              title: "hey it's a url"
+            }
+          ]
+        }
+      )
+    })
+  
+    it('Should render existing URLs from database', async () => {
+      render(<App />)
+      
+      const header = await waitFor(() => (screen.getByText('URL Shortener')))
 
-    expect(screen.getByPlaceholderText('Title...')).toBeInTheDocument()
-    expect(screen.getByPlaceholderText('URL to Shorten...')).toBeInTheDocument()
-    expect(screen.getByText('Shorten Please!')).toBeInTheDocument()
-    
-    expect(screen.getByText('No urls yet! Find some to shorten!')).toBeInTheDocument()
+      const url1 = {
+        title: screen.getByText("hey it's a url"),
+        short_url: screen.getByText("short.co"),
+        long_url: screen.getByText("iamaveryloooongurl.com")
+      }
+      expect(header).toBeInTheDocument()
+      expect(url1.title).toBeInTheDocument()
+      expect(url1.short_url).toBeInTheDocument()
+      expect(url1.long_url).toBeInTheDocument()      
+    })
+
   })
+
 })
