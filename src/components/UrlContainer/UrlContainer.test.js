5c9ffa227dbb0d6339b6e04b8ba38cd6beec60ba
Hanna Kim <hannakim91@gmail.com>
2020-11-10 10:45:10 -0700

    Unit test Url Container render

--- src/components/UrlContainer/UrlContainer.test.js
+++ src/components/UrlContainer/UrlContainer.test.js
@@ -1,9 +1,12 @@
+import React from 'react'
 import {render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import UrlContainer from './UrlContainer'
 
 describe('Url Container', () => {
   beforeEach(() => {
+  })
+  it('Should render a URL card', () => {
     const mockUrls = [
       {
         id: 1,
@@ -12,12 +15,15 @@ describe('Url Container', () => {
         title: "hey it's a url"
       }
     ]
-  })
-  it('Should render a URL card', () => {
+    
     render(
       <UrlContainer 
         urls={mockUrls}
       />
     )
+
+    expect(screen.getByText(mockUrls[0].title)).toBeInTheDocument()
+    expect(screen.getByText(mockUrls[0].long_url)).toBeInTheDocument()
+    expect(screen.getByText(mockUrls[0].short_url)).toBeInTheDocument()
   })
 })
