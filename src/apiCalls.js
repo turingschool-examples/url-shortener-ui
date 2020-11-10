084866e0ee630e37837f62f3599eb20454a31bd7
Hanna Kim <hannakim91@gmail.com>
2020-11-10 10:02:47 -0700

    Write POST request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = async (data) => {
+  const promise = await fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  return await promise.json()
+}
