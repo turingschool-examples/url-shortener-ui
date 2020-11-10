3558dd54e63aaf72b6c79dc808482d7eb3578269
Hanna Kim <hannakim91@gmail.com>
2020-11-10 10:56:33 -0700

    Test inputs on form changing

--- src/components/UrlForm/UrlForm.test.js
+++ src/components/UrlForm/UrlForm.test.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import {render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import UrlForm from './UrlForm'
 
 describe('Url Form', () => {
   beforeEach(() => {
   })
   it('Should render a URL form', () => {
-
     render(
       <UrlForm 
       />
     )
-    screen.debug()
+
     expect(screen.getByPlaceholderText('Title...')).toBeInTheDocument()
     expect(screen.getByPlaceholderText('URL to Shorten...')).toBeInTheDocument()
     expect(screen.getByText('Shorten Please!')).toBeInTheDocument()
+  })
+
+  it('Should update as data is entered by a user', () => {
+    render(
+      <UrlForm 
+      />
+    )
+    
+    userEvent.type(screen.getByPlaceholderText('Title...'),'bees knees')
+    expect(screen.getByDisplayValue('bees knees')).toBeInTheDocument()
+    userEvent.type(screen.getByPlaceholderText('URL to Shorten...'),'squeezy cheese')
+    expect(screen.getByDisplayValue('squeezy cheese')).toBeInTheDocument()
   })
 })
