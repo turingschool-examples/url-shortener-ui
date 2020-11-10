8f311986b790c7bdffff21e76eac5c19b1536515
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-11-10 11:56:37 -0700

    Create post for saveUrl

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,16 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const saveUrl = (value1, value2) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        long_url: value1,
+        title: value2
+      })
+    }).then(response => response.json)
+}
