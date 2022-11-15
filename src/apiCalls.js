0b1a60f86f11c80bc218ffbdaf69567bc780e127
MatthewPress <press.matt14@gmail.com>
2022-11-15 09:38:38 -0700

    Feat: add post request api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrls = (information) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(information)
+  })
+    .then(response => response.json())
+}
