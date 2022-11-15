6ec31b279b95fd5bee31972bee5cfbf8f7a5e99a
Emily Miles <ms.emily.j.miles@gmail.com>
2022-11-15 12:57:08 -0500

    Refactor to debug input entries

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './UrlContainer.css';
 
-const UrlContainer = props => {
-  const urlEls = props.urls.map(url => {
+const UrlContainer = ({ urls }) => {
+  const urlEls = urls.map(url => {
+    console.log(url);
     return (
-      <div className="url">
-        <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
-        <p>{url.long_url}</p>
+      <div className="url" key={ url.id }>
+        <h3>{ url.title }</h3>
+        <p>New shortened URL: <a href={ url.short_url } target="blank">{ url.short_url }</a></p>
+        <p>Previous URL: <a href={ url.long_url } target="blank">{ url.long_url }</a></p>
       </div>
-    )
+    );
   });
 
   return (
     <section>
       { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
     </section>
-  )
-}
+  );
+};
 
 export default UrlContainer;
