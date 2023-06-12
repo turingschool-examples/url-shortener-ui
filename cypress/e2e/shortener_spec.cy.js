2026a9edf3566362cd7a48d69f863e23ba98cfe3
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-06-12 13:14:29 -0400

    Add additional parameter to test for number of input elemetns inside of the form and update wording for it block

--- cypress/e2e/shortener_spec.cy.js
+++ cypress/e2e/shortener_spec.cy.js
@@ -7,19 +7,20 @@ describe('empty spec', () => {
     cy.visit('http://localhost:3000/')
   })
   
-  it.skip('Should display page title and the existing shortened URLs', () => {
+  it('Should display page title and the existing shortened URLs', () => {
     cy.get('h1').contains('URL Shortener')
     cy.get('div[class=url]').should('have.length', 3)
     cy.get('h3').contains('The coolest photo')
   })
 
-  it.skip('Should be able to view the Form all neccessary inputs', () => {
+  it('Should be able to view the Form and all neccessary inputs', () => {
+    cy.get('input').should('have.length', 2)
     cy.get('.title')
     cy.get('.long-url')
     cy.get('.submit-btn')
   })
 
-  it.skip('Should be able to fill out the form, and the information is reflected in the input fields', () => {
+  it('Should be able to fill out the form, and the information is reflected in the input fields', () => {
     cy.get('.title').type('Got places to be').should('have.value', 'Got places to be')
     cy.get('.long-url').type('https://images.unsplash.com/photo-1686276665537-8504356174ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60').should('have.value', 'https://images.unsplash.com/photo-1686276665537-8504356174ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60')
   })
