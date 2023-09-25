ff86920475aa7ba3848b5123de9f9e5764344393
Avery Berryman <averyberryman@gmail.com>
2023-09-25 09:36:29 -0600

    Clear errors from console and get URL data

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -4,9 +4,9 @@ import './UrlContainer.css';
 const UrlContainer = props => {
   const urlEls = props.urls.map(url => {
     return (
-      <div className="url">
+      <div className="url" key={url.id}>
         <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
+        <a href={url.short_url} target="blank" rel="noopener noreferrer">{url.short_url}</a>
         <p>{url.long_url}</p>
       </div>
     )
