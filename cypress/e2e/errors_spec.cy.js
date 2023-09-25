a94fc57d10ed06638c37919374afe23ab3181c9d
Judy Ye <judyy.ye@gmail.com>
2023-09-25 10:44:32 -0700

    Add test for incomplete input fields

--- cypress/e2e/errors_spec.cy.js
+++ cypress/e2e/errors_spec.cy.js
@@ -1,4 +1,4 @@
-describe('empty spec', () => {
+describe('errors', () => {
   beforeEach(()=> {
     cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
       statusCode: 200,
@@ -11,5 +11,18 @@ describe('empty spec', () => {
       statusCode: 500
     })
     cy.get('body').contains('p', 'Request failed - 500')
+  })
+  it('should show message for user to fill out all input fields', ()=>{
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+      statusCode: 200,
+      body: {
+        title: 'Ahhh',
+      }
+    })
+    cy.get('input[name="title"]').type('Ahhh').should('have.value', 'Ahhh')
+    .get('button').click()
+    cy.get('body').should('contain', 'p', 'Please fill out all input fields')
+    .get('input[name="long_url"]').type('h')
+    cy.get('Please fill out all input fields').should('not.exist')
   })
 })
