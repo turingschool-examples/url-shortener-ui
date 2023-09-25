947931b44e174dce71826cb86982b6e07c504915
Judy Ye <judyy.ye@gmail.com>
2023-09-25 08:44:50 -0700

    Add POST request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,17 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrls = (url) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(url)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error (`Post failed - ${response.status}`)
+      }
+      return response.json()
+    })
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { getUrls } from '../../apiCalls';
+import { getUrls, postUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
 function App () {
   const [urls, setUrls] = useState([]);
-  console.log('urls',urls)
+  
   useEffect(() => {
     getUrls()
     .then(data => setUrls(data.urls))
     .catch(err => console.log(err))
   }, [])
+
+  const addUrl = (inputURL) => {
+    postUrls(inputURL)
+    .then(data => {
+      setUrls([...urls, data])
+    })
+    .catch(err => console.log(err))
+  }
 
   return (
     <main className="App">
