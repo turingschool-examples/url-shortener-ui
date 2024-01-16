5b850b9005f913acbd9264d93587959a0cf2f4f3
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-01-16 13:17:12 -0500

    Finish iteration 2

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,13 @@ export const getUrls = () => {
       .then(response => response.json())
       .catch(error => console.error(error))
 }
+
+
+export const postUrl = (newUrl) => {
+  return fetch(`http://localhost:3001/api/v1/urls`, {
+    method: 'POST',
+    headers: { 'Content-Type': "application/json"},
+    body: JSON.stringify(newUrl)
+  }).then(response => response.json())
+  .catch(error => console.error(error))
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,37 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { getUrls } from "../../apiCalls";
+import { getUrls, postUrl } from "../../apiCalls";
 import UrlContainer from "../UrlContainer/UrlContainer";
 import UrlForm from "../UrlForm/UrlForm";
 
 function App() {
   const [urls, setUrls] = useState([]);
-
+ 
   useEffect(() => {
-    getUrls()
-      .then((data) => setUrls(data.urls))
-      .catch((error) => console.error(error));
+    fetchData()
   }, []);
+
+  const fetchData = () => {
+    getUrls()
+    .then(data => setUrls(data.urls))
+    .catch(err => console.error(err))
+  }
+
 
   const addUrl = (newUrl) => {
     setUrls([...urls, newUrl]);
+    postUrl(newUrl)
+    fetchData()
   };
+
+  
 
 
   return (
     <main className="App">
       <header>
         <h1>URL Shortener</h1>
-        <UrlForm addUrl={addUrl} />
+        <UrlForm addUrl={addUrl} postUrl={postUrl} />
       </header>
 
       <UrlContainer urls={urls} />
